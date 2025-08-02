6adb838de22a7289d3623a8751c01ccbed746ab5
silentfin <152957041+silentfin@users.noreply.github.com>
2025-08-03 00:43:44 +0530

    Perform general code cleanup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+MIN_PASSWORD_LENGTH = 4
 DEFAULT_PASSWORD_LENGTH = 8
 LOWER_ASCII_CHARACTERS = "abcdefghijklmnopqrstuvwxyz"
 UPPER_ASCII_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -11,7 +12,7 @@ def generate_password(length_of_password=DEFAULT_PASSWORD_LENGTH):
     """Generates a password of specified length
     Default:
         1. Length of password is 8
-        2. Conttains one lowercase, uppercase, digit and symbol
+        2. Contains one lowercase, uppercase, digit and symbol
 
     Returns:
         String: Returns the generated password
@@ -23,7 +24,7 @@ def generate_password(length_of_password=DEFAULT_PASSWORD_LENGTH):
     print("AcceptedChars: ", accepted_characters)
     print("LengthOfPass: ", length_of_password)
 
-    # atleast one uppercase, lowercase, digit, symbol
+    # atleast one uppercase, lowercase, digit, symbol = MIN_PASSWORD_LENGTH
     password = [
         random.choice(UPPER_ASCII_CHARACTERS),
         random.choice(LOWER_ASCII_CHARACTERS),
@@ -31,19 +32,25 @@ def generate_password(length_of_password=DEFAULT_PASSWORD_LENGTH):
         random.choice(SYMBOLS),
     ]
 
-    for _ in range(length_of_password - 4):
+    for _ in range(length_of_password - MIN_PASSWORD_LENGTH):
         password += random.choice(accepted_characters)
 
     # make it more complex by shuffling
     random.shuffle(password)
 
+    # returns the super strong password :)
     return "".join(password)
 
 
 if __name__ == "__main__":
     print("Generate Passwords :)")
-    n = int(input("Enter the length of password (min. 4): "))
-    if n < 4:
+    try:
+        n = int(input("Enter the length of password (min. 4): "))
+    except ValueError:
+        n = DEFAULT_PASSWORD_LENGTH
+    if n < MIN_PASSWORD_LENGTH:
         print("That will generate a weak password.")
-    else:
-        print(f"Generate Password: {generate_password(n)}")
+        print(f"Generating password of minimum length: {MIN_PASSWORD_LENGTH}")
+        n = MIN_PASSWORD_LENGTH
+
+    print(f"Generate Password: {generate_password(n)}")
