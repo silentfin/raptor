67b345f24bb01abdff26f9e63badd645ce1580a2
silentfin <152957041+silentfin@users.noreply.github.com>
2025-08-02 23:57:52 +0530

    Add shuffle & docstrings

--- main.py
+++ main.py
@@ -8,21 +8,32 @@ SYMBOLS = "~!@#$%^&*()"
 
 
 def generate_password(length_of_password=DEFAULT_PASSWORD_LENGTH):
+    """Generates a password of specified length
+    Default:
+        1. Length of password is 8
+        2. Conttains one lowercase, uppercase, digit and symbol
+
+    Returns:
+        None: For now, it just prints the password as is.
+    """
 
     accepted_characters = (
         LOWER_ASCII_CHARACTERS + UPPER_ASCII_CHARACTERS + DIGITS + SYMBOLS
     )
-    print(accepted_characters)
-    print(length_of_password)
+    print("AcceptedChars: ", accepted_characters)
+    print("LengthOfPass: ", length_of_password)
 
-    password = ""
+    password = []
     for i in range(length_of_password):
         password += random.choice(accepted_characters)
 
-    print(password)
+    # make it more complex by shuffling i guess
+    random.shuffle(password)
+    password = "".join(password)
+    print(f"Generate Password: {password}")
 
 
 if __name__ == "__main__":
-    print("Generate Passwords :) \n")
+    print("Generate Passwords :)")
     n = int(input("Enter the length of password: "))
     generate_password(n)
